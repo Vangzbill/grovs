ab3e1a062769a2bf8657a3bb34a1c2a4a34f997f
Vangzbill <mamadhan017@gmail.com>
2025-08-04 20:05:09 +0700

    fix: middleware without logging

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,4 @@
-import { NextRequest } from "next/server";
-
 export { default } from "next-auth/middleware";
-
-export function middleware(req: NextRequest) {
-  console.log('Middleware URL:', req.nextUrl.href);
-  return;
-}
 
 export const config = {
   matcher: ["/admin/((?!login).*)"],
